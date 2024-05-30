9a500754cf5d446172cae654954f22901b7f008e
Maxwell Neal <max.neal@seattlechildrens.org>
2024-05-29 17:32:23 -0700

    Updated script for execution on Mac.

--- perox_per_cell_macos.sh
+++ perox_per_cell_macos.sh
@@ -1,36 +1,52 @@
 #!/bin/bash
-file="/Users/betatester/SCRI/perox-per-cell/Experiment-0024.zvi"
-
-POdotcut=0.0064
-POminarea=1
-POmaxintensity=16383
-
+ 
 echo "Running perox-per-cell..."
-
-# Set output directory for z-projections
-zprojdircells="$file""_Zprojections/cells/"
-zprojmasksdir="$file""_ZprojectionMasks/"
+SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Source conda, otherwise conda init error messages may appear. If needed, edit the
 # location of conda.sh here using the output of the command
 # > conda info | grep -i 'base environment'
-source /opt/miniconda3/etc/profile.d/conda.sh 
+source /Users/maxneal/opt/anaconda3/etc/profile.d/conda.sh
 
-# Activate python 3.10.0 virtual environment
-conda activate pseg
+continue=1
 
-# Make z-projections and get peroxisome masks
-python zproject_get_perox_masks.py "$file" $POdotcut $POminarea $POmaxintensity
+while [ $continue -gt 0 ]
 
-# Deactivate python 3.10.0 environment
-conda deactivate
+do
+        # Activate virtual python environment for peroxisome segmentation
+		echo "Loading environment for peroxisome segmentation"
 
-# Activate Python 3.5.4 virtual environment
-conda activate cseg
+        conda activate pseg
 
-# Get cell masks
-python get_cell_masks_and_count.py "$file" "$zprojdircells" "$zprojmasksdir" $POdotcut $POminarea $POmaxintensity
+        # Make z-projections and get peroxisome masks
+        python zproject_get_perox_masks.py "$SCRIPTDIR"/
 
-# Deactivate Python 3.5 environment
-conda deactivate
+        # Deactivate python 3.10.0 environment
+        conda deactivate
 
+        # Set configuration file target location
+        configfile="$SCRIPTDIR"/ppc_job_config.json
+
+        # See if configuration file has been written out
+        if test -f "$configfile"; then
+
+                # Activate virtual python environment for cell segmentation
+				echo "Loading environment for cell segmentation"
+
+                conda activate cseg
+
+                # Get cell masks
+                python get_cell_masks_and_count.py "$configfile"
+
+                # Deactivate Python 3.5 environment
+                conda deactivate
+        fi
+
+        if test -f "$configfile"; then  # Make sure we clean up loose configuration files
+                rm "$configfile"
+                continue=1  # Program just ran, reopen for more jobs
+        else
+                continue=0  #Program was closed
+        fi
+
+done
